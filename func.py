4f488738c363e6ba6d754599d07a97afa5a73166
nikesh_g_gogia <nikesh.g.gogia@oracle.com>
2022-10-14 15:58:23 +0530

    Initial Commit

--- func.py
+++ func.py
@@ -9,7 +9,7 @@ ret = "Nikesh"
 # Import Code from Github or Orahub
 def ocr_lz():
     gurl = "https://github.com/ngogia20/oci_lz/archive/refs/heads/master.zip"
-    dir_name = '/tmp/'
+    dir_name = '/tmp'
     ret="200"
     wget.download(gurl,out=dir_name)
     # dir_name = '/tmp' #os.getcwd()
@@ -24,7 +24,7 @@ def ocr_lz():
             zip_ref.extractall(dir_name) # extract file to dir
             zip_ref.close() # close file
             os.remove(file_name) # delete zipped file
-    t = Terraform(working_dir=dir_name+"oci_lz-master")
+    t = Terraform(working_dir=dir_name+"/oci_lz-master")
 
     #t = Terraform(working_dir='.')
 
@@ -46,7 +46,30 @@ def handler(ctx, data: io.BytesIO=None):
         #body = json.loads(data.getvalue())
         #name = body.get("name")
         ret="Gogia"
-        ret=ocr_lz()
+        #ret=ocr_lz()
+        gurl = "https://github.com/ngogia20/oci_lz/archive/refs/heads/master.zip"
+        dir_name = '/tmp'
+        ret="200"
+        wget.download(gurl,out=dir_name)
+        # dir_name = '/tmp' #os.getcwd()
+        extension = ".zip"
+        ret="900"
+        os.chdir(dir_name) # change directory from working dir to dir with files
+        ret="100"
+        for item in os.listdir(dir_name): # loop through items in dir
+            if item.endswith(extension): # check for ".zip" extension
+                file_name = os.path.abspath(item) # get full path of files
+                zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+                zip_ref.extractall(dir_name) # extract file to dir
+                zip_ref.close() # close file
+                os.remove(file_name) # delete zipped file
+        ret="420"
+        t = Terraform(working_dir=dir_name+"/oci_lz-master")
+        return_code0, stdout0, stderr = t.init()
+        ret="334"
+        return_code0, stdout2, stderr2 = t.plan()
+        ret="gdfjgjf"
+        return_code1, stdout1, stderr1 = t.apply(skip_plan=True)
         #ret="hjkhjk"
     except (Exception, ValueError) as ex:
         print(str(ex), flush=True)
