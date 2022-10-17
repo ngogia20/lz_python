612fd8b4323b2a6578da9b1dc6fe223c14c8f108
nikesh_g_gogia <nikesh.g.gogia@oracle.com>
2022-10-17 21:13:34 +0530

    Initial Commit

--- func.py
+++ func.py
@@ -26,6 +26,8 @@ def ocr_lz():
             zip_ref.close() # close file
             os.remove(file_name) # delete zipped file
     ret="22222"
+    os.system('chmod +x /tmp/oci_lz-master')
+    os.system('chmod +x /tmp/oci_lz-master/*')
     os.chdir("oci_lz-master")
     dir_list = os.listdir(os.getcwd())
     print(dir_list,flush=True)
