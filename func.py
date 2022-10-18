684f500879a8d7dad3d99b6f59cc3b43765c8e93
nikesh_g_gogia <nikesh.g.gogia@oracle.com>
2022-10-18 15:18:54 +0530

    Initial Commit

--- func.py
+++ func.py
@@ -12,21 +12,21 @@ def ocr_lz():
     os.system('ls -l') # /tmp/oci_lz
     dirlist=os.listdir(os.getcwd()) #[]
     print(dirlist,flush=True)
-    dir_name='/tmp'
-    gurl = "https://github.com/ngogia20/oci_lz/archive/refs/heads/master.zip"
-    ret="200"
-    wget.download(gurl,out=dir_name)
+    #dir_name='/tmp'
+    #gurl = "https://github.com/ngogia20/oci_lz/archive/refs/heads/master.zip"
+    #ret="200"
+    #wget.download(gurl,out=dir_name)
     # dir_name = '/tmp' #os.getcwd()
-    extension = ".zip"
-    ret="900"
-    ret="100"
-    for item in os.listdir(dir_name): # loop through items in dir
-        if item.endswith(extension): # check for ".zip" extension
-            file_name = os.path.abspath(item) # get full path of files
-            zip_ref = zipfile.ZipFile(file_name) # create zipfile object
-            zip_ref.extractall(dir_name) # extract file to dir
-            zip_ref.close() # close file
-            os.remove(file_name) # delete zipped file
+    #extension = ".zip"
+    #ret="900"
+    #ret="100"
+    #for item in os.listdir(dir_name): # loop through items in dir
+        #if item.endswith(extension): # check for ".zip" extension
+        #    file_name = os.path.abspath(item) # get full path of files
+        #    zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+        #    zip_ref.extractall(dir_name) # extract file to dir
+        #    zip_ref.close() # close file
+        #    os.remove(file_name) # delete zipped file
     #try:
     #    os.system('terraform init')
     #    os.system('terraform plan')
@@ -35,7 +35,8 @@ def ocr_lz():
     #    print("SEE ERROR",flush=True)
     #    print(str(e2), flush=True)
     os.system('ls -l')
-    os.system('cd /tmp/oci_lz-master')
+    #os.system('cd /tmp/oci_lz-master')
+    os.system('terraform -chdir=/function/oci_lz init')
     #print("With Siva Sir Nikesh 3",flush=True)
     #print(comm4,flush=True)
 
